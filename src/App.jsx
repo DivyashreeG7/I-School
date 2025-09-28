b896ce453b0af667132ba45ac334f3b1053d6a80
Divyashree G <divyashreeg2005@gmail.com>
2025-09-28 12:40:42 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,50 +5,39 @@ import Announcementbar from './components/AnnouncementBar';
 import AnnouncementBanner from './components/AnnouncementBanner';
 import About from './components/About';
 import Academics from './components/Academics';
-import Announcements from './components/Announcements'; 
+import Footer from './components/Footer';
 import Gallery from './components/Gallery';
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        {/* Announcement Banner (Modal) - Shows on page load */}
+      <div className="App min-h-screen flex flex-col">
         <AnnouncementBanner />
-        
         <Routes>
-          {/* Home page with scrollable sections */}
-          <Route path="/" element={
-            <>
-              <Nav />
-              <Announcementbar />
-              <div className="scroll-container">
-                <section id="home" className="min-h-screen">
-                  <Home />
-                </section>
-                <section id="about" className="min-h-screen">
-                  <About />
-                </section>
-                <section id="academics" className="min-h-screen">
-                  <Academics />
-                </section>
-                 <section id="announcements" className="min-h-screen">
-                  <Announcements />
-                </section>
-                <section id="gallery" className="min-h-screen">
-                  <Gallery />
-                </section>
-              </div>
-            </>
-          } />
-          
-        
-          {/* <Route path="/contact" element={
-            <>
-              <Nav />
-              <Announcements />
-              <div>Contact Page</div>
-            </>
-          } /> */}
+          <Route
+            path="/"
+            element={
+              <>
+                <Nav />
+                <Announcementbar />
+                <div className="scroll-container flex-1">
+                  <section id="home" className="min-h-screen">
+                    <Home />
+                  </section>
+                  <section id="about" className="min-h-screen">
+                    <About />
+                  </section>
+                  <section id="academics" className="min-h-screen">
+                    <Academics />
+                  </section>
+                  <section id="gallery" className="min-h-screen">
+                    <Gallery />
+                  </section>
+                </div>
+                <Footer />
+              </>
+            }
+          />
         </Routes>
       </div>
     </Router>
